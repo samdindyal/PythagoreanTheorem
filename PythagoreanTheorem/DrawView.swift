99b239e78376dc2378a897767037a1c83f0a6c33
Sam Dindyal <sam.dindyal@me.com>
2018-07-30 18:42:26 -0400

    Adds view identities and creates view controller.

---
+++ PythagoreanTheorem/DrawView.swift
@@ -0,0 +1,16 @@
+//
+//  DrawView.swift
+//  PythagoreanTheorem
+//
+//  Created by Sam Dindyal on 2018-07-30.
+//  Copyright © 2018 Sam Dindyal. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        
+    }
+}
---
+++ PythagoreanTheorem/PythagoreanTheoremController.swift
@@ -0,0 +1,18 @@
+//
+//  PythagoreanTheoremController.swift
+//  PythagoreanTheorem
+//
+//  Created by Sam Dindyal on 2018-07-30.
+//  Copyright © 2018 Sam Dindyal. All rights reserved.
+//
+
+import UIKit
+
+class PythagoreanTheoremController: UIViewController {
+    @IBOutlet var aField:UITextField!
+    @IBOutlet var bField:UITextField!
+    @IBOutlet var cField:UITextField!
+    
+    @IBOutlet var canvas:DrawView!
+    
+}
